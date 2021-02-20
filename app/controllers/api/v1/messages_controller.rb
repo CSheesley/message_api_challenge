c07bbc8cda40b54ed96a77791b42d2a069bec214
Corey Sheesley <corey.sheesley@gmail.com>
2021-02-20 15:30:50 -0700

    Handle create requests with missing params - provide error feedback

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,9 +3,15 @@ class Api::V1::MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     if message.save
+<<<<<<< Updated upstream
       render status: 201, json: { success: 'message created' }
     else
       # render status: 422, json: { message.errors }
+=======
+      render status: 201, json: { success: 'Message created' }
+    else
+      render status: 422, json: { errors: message.errors.full_messages.join(", ") }
+>>>>>>> Stashed changes
     end
   end
 
--- spec/requests/api/v1/messages_request_spec.rb
+++ spec/requests/api/v1/messages_request_spec.rb
@@ -16,15 +16,23 @@ RSpec.describe Api::V1::MessagesController, type: :request do
           post '/api/v1/messages', params: valid_params
         }.to change { Message.count }.by(1)
 
+<<<<<<< Updated upstream
         creation_response = JSON.parse(response.body, symbolize_names: true)
 
         expect(response.status).to eq(201)
         expect(creation_response[:success]).to eq('message created')
+=======
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(201)
+        expect(parsed_response[:success]).to eq('Message created')
+>>>>>>> Stashed changes
       end
     end
 
     context 'with invalid or missing params' do
       it 'does not create a Message object - missing :body' do
+<<<<<<< Updated upstream
       end
 
       it 'does not create a Message object - missing :recipient' do
@@ -35,6 +43,71 @@ RSpec.describe Api::V1::MessagesController, type: :request do
 
       it 'does not create a Message object - sending and illegal param' do
       end
+=======
+        missing_body_params = {
+          recipient: 'abbey',
+          sender: 'corey'
+        }
+
+        expect {
+          post '/api/v1/messages', params: missing_body_params
+        }.to_not change { Message.count }
+
+        creation_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(422)
+        expect(creation_response[:errors]).to eq("Body can't be blank")
+      end
+
+      it 'does not create a Message object - missing :recipient' do
+        missing_body_params = {
+          body: 'a short message',
+          sender: 'corey'
+        }
+
+        expect {
+          post '/api/v1/messages', params: missing_body_params
+        }.to_not change { Message.count }
+
+        creation_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(422)
+        expect(creation_response[:errors]).to eq("Recipient can't be blank")
+      end
+
+      it 'does not create a Message object - missing :sender' do
+        missing_body_params = {
+          body: 'a short message',
+          recipient: 'abbey',
+        }
+
+        expect {
+          post '/api/v1/messages', params: missing_body_params
+        }.to_not change { Message.count }
+
+        creation_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(422)
+        expect(creation_response[:errors]).to eq("Sender can't be blank")
+      end
+
+      # it 'does not create a Message object - sending and illegal param' do
+      #   missing_body_params = {
+      #     body: 'a short message',
+      #     sender: 'corey',
+      #     recipient: 2,
+      #   }
+      #
+      #   expect {
+      #     post '/api/v1/messages', params: missing_body_params
+      #   }.to_not change { Message.count }
+      #
+      #   creation_response = JSON.parse(response.body, symbolize_names: true)
+      #
+      #   expect(response.status).to eq(422)
+      #   expect(creation_response[:errors]).to eq("Sender can't be blank")
+      # end
+>>>>>>> Stashed changes
     end
 
   end
