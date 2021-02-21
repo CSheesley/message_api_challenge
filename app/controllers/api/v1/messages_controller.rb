5a8778b744852b4ccaca301828a88c14f95cba28
Corey Sheesley <corey.sheesley@gmail.com>
2021-02-20 18:45:33 -0700

    Add MessagesController#index and route - plan logic with pseudo code

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::MessagesController < ApplicationController
 
+  def index
+    messages = Search::MessagesFilter.new(message_params)
+    render status: 200, json: MessagesSerializer.new(messages).formatted_data
+  end
+
   def create
     message = Message.new(message_params)
-
     message.save ? success_response : error_response(message)
   end
 
