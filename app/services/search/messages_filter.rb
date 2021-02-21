684235dc34c55df59e23c8238221788826def5e5
Corey Sheesley <corey.sheesley@gmail.com>
2021-02-21 00:01:33 -0700

    Remove unused :body attribute from Search::MessagesFilter

--- app/services/search/messages_filter.rb
+++ app/services/search/messages_filter.rb
@@ -3,7 +3,6 @@ class Search::MessagesFilter
   def initialize(message_params)
     @recipient = message_params[:recipient]
     @sender = message_params[:sender]
-    @body = message_params[:body]
   end
 
   def results
